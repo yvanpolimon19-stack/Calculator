c06fd8ec0ad6586eee241efdd8fa2cce5e60dfa8
Yvan Polimon <yvanpolimon19@gmail.com>
2026-03-10 19:36:03 +0400

    The calculator now works and does the basic functions

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+const display = document.getElementById("display");
+const digits = document.querySelectorAll(".digit");
+const operators = document.querySelectorAll(".operator");
+const buttons = document.querySelectorAll(".button");
+const equalBtn = document.getElementById("equal");
+const digitsArr = [];
+const operatorsArr = [];
+let operator;
+let firstNumber;
+let secondNumber;
+
+for (let i = 0; i < operators.length; i ++) {
+    operatorsArr[i] = operators[i].textContent;
+}
+for (let i = 0; i < digits.length; i ++) {
+    digitsArr[i] = digits[i].textContent;
+}
+
+
 function add(a, b) {
     return a + b;
 }
@@ -16,15 +35,43 @@ function multiply(a, b) {
 
 const operate = function(a, b, operator) {
     if (operator === '+') {
-        add(a, b);
+        return add(a, b);
     }
     else if (operator === '-') {
-        subtract(a, b);
+        return subtract(a, b);
     }
     else if (operator === '*') {
-        multiply(a, b);
+        return multiply(a, b);
     }
     else if (operator === '/') {
-        divide(a, b);
+        return divide(a, b);
     }
 }
+
+const handleClick = function (e) {
+    const buttonContent = e.target.textContent;
+    if(operatorsArr.includes(buttonContent)) {
+        firstNumber = parseInt(display.textContent);
+        operator = buttonContent;
+        display.textContent = '';
+    }
+    else if (digitsArr.includes(buttonContent)) {
+        display.textContent += buttonContent;
+    }
+    else if (buttonContent === equalBtn.textContent) {
+        
+        secondNumber = parseInt(display.textContent);
+
+        display.textContent = operate(firstNumber, secondNumber, operator)       
+        
+    }
+}
+
+function update () {
+    buttons.forEach(button =>{
+        button.addEventListener("click", handleClick)
+    })
+}
+
+
+update();
